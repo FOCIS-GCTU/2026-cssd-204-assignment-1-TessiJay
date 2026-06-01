cf0c3e3e997ab0170989f76ed475b2f8891c6834
TessiJay <2425400419@live.gctu.edu.gh>
2026-06-01 23:11:54 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
+# File: initials.py
+# Description: My Printed Initials using Python
+# Assignment Number: 02 
+# 
+# Name: Felix Kwadwo Junior Tamakloe
+# STUDENT ID:  2425400419
+# Email: fastechlifetimequotes@gmail.com
+# Grader: Augustus Buckman 
+# 
+# On my honor, Felix Kwadwo Junior Tamakloe, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
-
+    # This function prints the initials F.K.J in a stylized large format using strings and print functions.
+    print()
+    print("...F.K.J.")
+    print()
+    print("...FFFFFFFFFFFF........KKKK....KKKK.........JJJJJJJJJJ.....")
+    print("...FFFFFFFFFFFF........KKKK...KKKK..........JJJJJJJJJJ.....")
+    print("...FFFF................KKKK..KKKK.................JJJJ.....")
+    print("...FFFF................KKKK.KKKK..................JJJJ.....")
+    print("...FFFFFFFFFFFF........KKKKKKKK...................JJJJ.....")
+    print("...FFFFFFFFFFFF........KKKKKKKK...................JJJJ.....")
+    print("...FFFF................KKKK.KKKK...........JJJJ...JJJJ.....")
+    print("...FFFF................KKKK..KKKK..........JJJJ...JJJJ.....")
+    print("...FFFF...........**...KKKK...KKKK....**...JJJJJJJJJJJ...**")
+    print("...FFFF...........**...KKKK....KKKK...**....JJJJJJJJJ....**")
+    print()
 
 
 main()
